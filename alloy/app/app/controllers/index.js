833e1e9487781d2eb7b7d90b4e4c3e468803324e
Luis Pedro Ferreira <luispedro.ezy@gmail.com>
2015-01-08 13:20:04 +0100

    New widget :: i18nWidget

--- alloy/app/app/controllers/index.js
+++ alloy/app/app/controllers/index.js
@@ -1,6 +1,7 @@
 
-// Import i18n module
-var L = require('i18nModule').i18n($.__controllerPath).getString;
+// TODO: A widget instead of a module
+
+var L = Alloy.Globals.i18n.controller($).getStrings;
 
 $.window_title.text = L('window_title');
 $.open_button.title = L('button_label');
--- alloy/app/app/controllers/welcomeWindow.js
+++ alloy/app/app/controllers/welcomeWindow.js
@@ -1,8 +1,11 @@
 var args = arguments[0] || {};
 
-var L = require('i18nModule').i18n($.__controllerPath).getString;
 
-$.window_title.text = L('window_title');
+//var L = require('i18nModule').i18n($.__controllerPath).getString;
+
+var L = Alloy.Globals.i18n.controller($).getStrings;
+
+$.window_title.text = L('window_title1');
 $.close_button.title = L('close_button_label');
 
 function closeWindow(e){
